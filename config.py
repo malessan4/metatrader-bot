d4c1b0aaff8631a8da86eeddfecdc2119d6b14b8
malessan4 <matialess@gmail.com>
2026-07-17 01:09:24 -0300

    se cargan los datos por el .env

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # Carga las variables del archivo .env
+
 import MetaTrader5 as mt5
 
 # Configuración General
 SYMBOL = "XAUUSD" # IMPORTANTE: Ajustar al nombre exacto de tu broker si es distinto (ej. "XAUUSD.a" o "Gold")
 LOT_SIZE = 0.02
-MAGIC_NUMBER = 123456 # Identificador único para las operaciones del bot
+MAGIC_NUMBER = int(os.getenv("MAGIC_NUMBER", 123456)) # Identificador único para las operaciones del bot
 DEVIATION = 20 # Desviación permitida (slippage) en puntos
 
 # Temporalidades a monitorear
@@ -18,3 +23,7 @@ ATR_PERIOD = 14
 ATR_MULTIPLIER_FVG = 0.5
 SMA_BODY_PERIOD = 20
 OB_BODY_MULTIPLIER = 2.0
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
+MAGIC_NUMBER = int(os.getenv("MAGIC_NUMBER", 123456))
